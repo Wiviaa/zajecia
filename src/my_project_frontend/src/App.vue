8303e9ce902f5a81d5ee0abae66d1fcd5398102e
Wiviaa :o <celmer.nikola@wp.pl>
2024-12-13 11:07:36 +0000

    12:07

--- src/my_project_frontend/src/App.vue
+++ src/my_project_frontend/src/App.vue
@@ -3,12 +3,17 @@ import { ref } from 'vue';
 import { my_project_backend } from 'declarations/my_project_backend/index';
 
 const rates = ref([])
+const iloscWaluty = ref(0)
 
 const getDataFromNBP = async () => {
   const res = await fetch("https://api.nbp.pl/api/exchangerates/tables/A/?format=json")
   const jsonData = await res.json();
   console.log(jsonData)
   rates.value = jsonData[0].rates
+}
+
+const kupWalute = async (index) => {
+  console.log(rates.value[index])
 }
 
 getDataFromNBP()
@@ -24,11 +29,14 @@ getDataFromNBP()
         <th> Nazwa waluty </th>
         <th> Kod waluty </th>
         <th> Cena </th>
+        <th> Ilość waluty do zakupu</th>
       </tr>
-      <tr v-for="rate in rates">
+      <tr v-for="(rate, index) in rates">
         <td> {{ rate.currency }} </td>
         <td> {{ rate.code }} </td>
         <td> {{ rate.mid }} </td>
+        <td> <input type="number" v-model="iloscWaluty"></td>
+        <td> <button @click="kupWalute(index)"> Kup </button></td>
       </tr>
     </table>
   </main>
