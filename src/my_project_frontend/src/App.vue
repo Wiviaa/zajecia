761e953fc991b9e49c14886a5086569fb0a30d93
Wiviaa :o <celmer.nikola@wp.pl>
2024-12-13 10:42:39 +0000

    11.42

--- src/my_project_frontend/src/App.vue
+++ src/my_project_frontend/src/App.vue
@@ -2,12 +2,13 @@
 import { ref } from 'vue';
 import { my_project_backend } from 'declarations/my_project_backend/index';
 
+const rates = ref([])
+
 const getDataFromNBP = async () => {
-  const res = await fetch ("https://api.nbp.pl/api/exchangerates/tables/A/?format=json")
+  const res = await fetch("https://api.nbp.pl/api/exchangerates/tables/A/?format=json")
   const jsonData = await res.json();
   console.log(jsonData)
-  const rates = jsonData[0].rates
-  console.log(rates)
+  rates.value = jsonData[0].rates
 }
 
 getDataFromNBP()
@@ -18,7 +19,17 @@ getDataFromNBP()
     <img src="/logo2.svg" alt="DFINITY logo" />
     <br />
     <br />
-    <input v-model="input" type="text" />
-    {{ input }} 
+    <table>
+      <tr>
+        <th> Nazwa waluty </th>
+        <th> Kod waluty </th>
+        <th> Cena </th>
+      </tr>
+      <tr v-for="rate in rates">
+        <td> {{ rate.currency }} </td>
+        <td> {{ rate.code }} </td>
+        <td> {{ rate.mid }} </td>
+      </tr>
+    </table>
   </main>
 </template>
